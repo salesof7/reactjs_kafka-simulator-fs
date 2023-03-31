9bfdf15c73787877fae833d0f65fc2354d6dae39
salesof7 <jpsaless2002@gmail.com>
2023-03-31 15:31:19 -0300

    feat: implement connection with nest-api

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -4,6 +4,8 @@ import ExpandLessIcon from "@mui/icons-material/ExpandLess";
 import { FormEvent, useEffect, useState } from "react";
 import { Route } from "../domain/models";
 
+const API_URL = import.meta.env.VITE_API_URL;
+
 export function Form() {
   const [routes, setRoutes] = useState<Route[]>([]);
   const [clicked, setClicked] = useState<boolean>(false);
@@ -11,28 +13,12 @@ export function Form() {
   const [runningProcess, setRunningProcess] = useState<string[]>([]);
 
   useEffect(() => {
-    const populate: Route[] = [
-      {
-        _id: "1",
-        title: "Rota I: Epic Route ABC",
-        startPosition: { lat: -15.82594, lng: -47.92923 },
-        endPosition: { lat: -15.82942, lng: -47.92765 },
-      },
-      {
-        _id: "2",
-        title: "Rota II: Epic Route BCA",
-        startPosition: { lat: -15.82449, lng: -47.92756 },
-        endPosition: { lat: -15.8276, lng: -47.92621 },
-      },
-      {
-        _id: "3",
-        title: "Rota III: Epic Route CAB",
-        startPosition: { lat: -15.82331, lng: -47.92588 },
-        endPosition: { lat: -15.82758, lng: -47.92532 },
-      },
-    ];
-
-    setRoutes(populate);
+    const fetchRoutes = async () => {
+      const data = await fetch(`${API_URL}/routes`);
+      const dataJSON: Route[] = await data.json();
+      setRoutes(dataJSON);
+    };
+    fetchRoutes().catch(console.error);
   }, []);
 
   function handleSubmit(event: FormEvent) {
